d8297bc674a26b81be3167f32d6d93f935eb0a1b
KarenNgala <ngalalalu@gmail.com>
2020-06-27 01:15:49 +0300

    Saving credentials

--- locker.py
+++ locker.py
@@ -2,13 +2,20 @@ class Credentials:
     '''
     class that creates new account credentials
     '''
-    credentials=[]
+    account_credentials=[] #list of accounts
 
     def __init__(self, account_name, username, password):
         '''
-        method to initialize object
+        method to initialize account object
         '''
         self.account_name = account_name
         self.username = username
         self.password = password
     
+    # new_account is an instance of Credentials class. Contains details for one account
+
+    def save_account(self):
+        '''
+        method to save account object
+        '''
+        self.account_credentials.append(Credentials)
--- test_locker.py
+++ test_locker.py
@@ -4,27 +4,30 @@ from locker import Credentials
 class LockerTest(unittest.TestCase):
     def setUp(self):
         '''
-        run before each test
+        method run before each test
         '''
         self.new_account = Credentials("instagram","linda","myPassword")
-        print("setup")
 
     def tearDown(self):
         '''
-        run after each test
+        method run after each test
         '''
         self.new_account = []
-        print("teardown")
 
     def test_init(self):
         '''
-        test to check initialization
+        test to check for proper initialization
         '''
         self.assertEqual(self.new_account.account_name,"instagram")
         self.assertEqual(self.new_account.username,"linda")
         self.assertEqual(self.new_account.password,"myPassword")
-        print("setup")
 
+    def test_save_account(self):
+        '''
+        test method that checks if account has been saved
+        '''
+        self.new_account.save_account()
+        self.assertEqual(len(Credentials.account_credentials),1)
 
 if __name__ == "__main__":
     unittest.main()
