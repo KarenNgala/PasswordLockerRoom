650fcf18953e86b9b16847e3e4a42132b7625ad5
KarenNgala <ngalalalu@gmail.com>
2020-06-29 21:36:25 +0300

    clean up test code and add doc strings

--- test_locker.py
+++ test_locker.py
@@ -6,36 +6,30 @@ from locker import User
 class UserTest(unittest.TestCase):
     def setUp(self):
         '''
+        method run before each user test
         '''
         self.new_user = User("Human", "orAmI?")
-        print("//////////////////////////////")
-        print("setup-user")
 
     def tearDown(self):
+        '''
+        method called after each user test
+        '''
         User.data_user = []
-        print("teardown-user")
 
     def test_init(self):
         '''
+        test method to check if user class is initialized correctly
         '''
         self.assertEqual(self.new_user.owner,"Human")
         self.assertEqual(self.new_user.key, "orAmI?")
-        print("init-user")
-
+        
     def test_save_user(self):
         '''
+        test method to test if user has been saved into class list
         '''
         self.new_user.save_user()
         self.assertEqual(len(User.data_user),1)
-        print("user saved")
 
-    # def test_number_of_users(self):
-    #     '''
-    #     '''
-    #     self.new_user.save_user()
-    #     another_user = User("Oliver", "tToTheTwist")
-    #     another_user.save_user()
-        
 
 class LockerTest(unittest.TestCase):
     def setUp(self):
@@ -43,15 +37,12 @@ class LockerTest(unittest.TestCase):
         method run before each test
         '''
         self.new_account = Credentials("instagram","linda","myPassword")
-        print("*****************************")
-        print("setup")
 
     def tearDown(self):
         '''
         method run after each test
         '''
         Credentials.account_credentials = []
-        print("teardown")
 
     def test_init(self):
         '''
@@ -60,7 +51,6 @@ class LockerTest(unittest.TestCase):
         self.assertEqual(self.new_account.account_name,"instagram")
         self.assertEqual(self.new_account.username,"linda")
         self.assertEqual(self.new_account.password,"myPassword")
-        print("init")
 
     def test_save_account(self):
         '''
@@ -68,7 +58,6 @@ class LockerTest(unittest.TestCase):
         '''
         self.new_account.save_account()
         self.assertEqual(len(Credentials.account_credentials),1)
-        print("save1")
 
     def test_save_multiple(self):
         '''
@@ -78,7 +67,6 @@ class LockerTest(unittest.TestCase):
         another_account = Credentials("Twitter", "Bob", "Burgers123")
         another_account.save_account()
         self.assertEqual(len(Credentials.account_credentials),2)
-        print("save2")
         
     def test_delete_account(self):
         '''
@@ -87,41 +75,35 @@ class LockerTest(unittest.TestCase):
         self.new_account.save_account()
         another_account = Credentials("Twitter", "Bob", "Sagers")
         another_account.save_account()
-        print(len(Credentials.account_credentials)) #test
         self.new_account.delete_account()
         self.assertEqual(len(Credentials.account_credentials),1)
-        print(len(Credentials.account_credentials)) #test
-
     
     def test_password_autogenerate(self):
         '''
+        test method to test auto generation of passwords
         '''
         self.new_account.save_account()
         another_account = Credentials("Twitter", "Bob")
         another_account.save_account()
-        print("pass generate: " + another_account.password)
 
     def test_dislay_accounts(self):
         '''
+        test method to check displaying of accounts
         '''
         self.new_account.save_account()
         another_account = Credentials("Twitter","Bobby", "Brown-asf")
         another_account.save_account()
         self.assertEqual(Credentials.display_accounts(),Credentials.account_credentials)
-        for acc in Credentials.account_credentials:
-            print(acc.account_name)
-        print("display")
 
     def test_search_accounts(self):
         '''
+        test method to test search functionality
         '''
         self.new_account.save_account()
         another_account = Credentials("Twitter", "Bob", "Burgers@123")
         another_account.save_account()
         self.assertEqual(another_account,Credentials.search_accounts("Twitter"))
-        print("search")
 
 
-        
 if __name__ == "__main__":
     unittest.main()
